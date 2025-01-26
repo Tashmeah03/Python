3edec4647307ab228c4f89b496a63503b7b43a9f
Faiza Tashmeah <faizatashmeah@gmail.com>
2025-01-26 19:33:58 +0600

    Add files via upload

---
+++ for_loop_dictionary.py
@@ -0,0 +1,6 @@
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw', 'austria':'vienna' }
+          
+# Iterate over europe
+for key, value in europe.items():
+   print("the capital of " + key + " is " + value)
